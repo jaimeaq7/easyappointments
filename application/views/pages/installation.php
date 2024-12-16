2e1edb3156b4c9b6f2176f6fc637a451ee34776d
jaimeaq7 <jaimeaq@protonmail.com>
2024-12-16 23:58:44 +0100

    updates

--- application/views/pages/installation.php
+++ application/views/pages/installation.php
@@ -17,25 +17,19 @@
 
 <header>
     <div class="container">
-        <h1 class="page-title">Easy!Appointments Installation</h1>
+        <h1 class="page-title">Installation</h1>
     </div>
 </header>
 
 <div class="content container">
     <div class="welcome">
-        <h3>Welcome to the Easy!Appointments installation page.</h3>
+        <h3>Welcome to the installation page.</h3>
         <p>
-            This page will help you set the main settings of your Easy!Appointments installation. You will be able to
+            This page will help you set the main settings of your installation. You will be able to
             edit these settings and many more in the backend session of your system. Remember to use the
-            <strong class="text-primary"><?= site_url('user/login') ?></strong> URL to connect to the backend section
-            of Easy!Appointments.
+            <strong class="text-primary"><?= site_url('user/login') ?></strong> URL to connect to the backend section.
 
-            If you face any problems during the usage of Easy!Appointments you can always check the
-            <a href="https://easyappointments.org/docs.html">Documentation</a> and
-            <a href="https://groups.google.com/group/easy-appointments">Support Group</a> for getting help. You may also
-            submit new issues on
-            <a href="https://github.com/alextselegidis/easyappointments/issues">GitHub Issues</a>
-            in order to help our development process.
+            
         </p>
     </div>
 
@@ -175,19 +169,13 @@
 
     <br>
 
-    <div class="mb-2">
-        <h3>License</h3>
-        Easy!Appointments is licensed under the <span class="badge bg-secondary">GPL-3.0 license</span>. By using the
-        code
-        of Easy!Appointments in any way <br> you agree with the terms described in the following url:
-        <a href="https://www.gnu.org/licenses/gpl-3.0.en.html">https://www.gnu.org/licenses/gpl-3.0.en.html</a>
-    </div>
+    
 
     <br>
 
     <button type="button" id="install" class="btn btn-primary">
         <i class="icon-white icon-ok me-2"></i>
-        Install Easy!Appointments
+        Install
     </button>
 </div>
 
